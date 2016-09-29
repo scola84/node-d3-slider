1c367d392b0468669a514cd36bed1653a0e93790
M.C. van den Boogaart <scola@kvdb.name>
2016-09-29 15:11:13 +0200

    Update dependencies and scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 import buble from 'rollup-plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'index.js',
   format: 'umd',
   globals: {
-    'd3-selection': 'd3'
+    'd3-selection': 'd3',
+    'd3-selection-multi': 'd3',
+    'd3-transition': 'd3'
   },
   plugins: [
+    resolve({
+      'jsnext:main': true
+    }),
     buble()
   ]
 };
